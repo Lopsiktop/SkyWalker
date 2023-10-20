4f7b9597176ec64d089c47bb4956873027795f09
student <student@B2-411-06>
2023-10-20 08:59:58 +0400

    Добавил окно авторизации без функционала

---
+++ Data/SkyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyWalker.Models;
+
+namespace SkyWalker.Data;
+
+public class SkyDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SkyWalkerDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
